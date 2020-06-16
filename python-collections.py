c6b6f92aa2e49830f081d76dd91566ae0412b128
santosh1561995 <sdevkate17@gmail.com>
2020-06-16 11:59:55 +0530

    Added file of python collection examples

---
+++ python-collections.py
@@ -0,0 +1,32 @@
+print("Python collections such as List,Tuple,Set,Dictionary")
+
+print("===============list in python===================")
+list=['SANTOSH','NITIN','RAINA','HARIKA','JULIYA','NISHAN']
+
+for x in list:
+    print(x) 
+
+
+print("===============Tuple in python==================") 
+
+tuple =(1,'One',"Two",3.3)
+for x in tuple:
+    print(x)
+
+
+
+
+print("==============Set in python ====================")
+set={'One','Two','Three','One'}
+
+for x in set:
+    print(x)
+
+
+print("==============Dictionary in python==============")
+
+dict={"Key":"Value",
+        "one":1,
+        "two":2}
+
+print(dict)
