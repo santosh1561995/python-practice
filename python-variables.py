52f932396f7888a78367435b7f3bf2261d59db85
santosh1561995 <sdevkate17@gmail.com>
2020-06-16 11:23:29 +0530

    Added python variables and datatypes example file

---
+++ python-variables.py
@@ -0,0 +1,38 @@
+print("For the datatypes in python an variable declaration")
+#in python data type will be calculated automtically no need to declare explicitly
+
+#integer type 
+x=5
+print(x)
+
+#float
+f=5.5
+print(f)
+
+#multiple assignment to variable
+a=b=c=10
+print(a,b,c)
+
+
+#assign values in one line 
+a,b=1,"India"
+print(a,b)
+
+
+#Swapping the values 
+x,y="X","Y"
+y,x=x,y 
+print(x,y)
+
+
+
+#Strings in python 
+
+x='String 1'
+y="String 2"
+
+print(x,y)
+
+
+
+
